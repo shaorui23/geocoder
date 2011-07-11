13921c5ff56efbcfbb5c8a3188dcd9fb194cdf1b
shao <shao@shao-desktop.(none)>
2011-07-11 10:35:10 +0800

    first change

---
+++ app/controllers/locations_controller.rb
@@ -0,0 +1,82 @@
+class LocationsController < ApplicationController
+  # GET /locations
+  # GET /locations.xml
+  def index
+    if params[:search].present?
+    @locations = Location.near(params[:search], 500, :order => :distance)
+    else
+      @locations = Location.all
+    end
+  end
+
+  # GET /locations/1
+  # GET /locations/1.xml
+  def show
+    @location = Location.find(params[:id])
+
+    respond_to do |format|
+      format.html # show.html.erb
+      format.xml  { render :xml => @location }
+    end
+  end
+
+  # GET /locations/new
+  # GET /locations/new.xml
+  def new
+    @location = Location.new
+
+    respond_to do |format|
+      format.html # new.html.erb
+      format.xml  { render :xml => @location }
+    end
+  end
+
+  # GET /locations/1/edit
+  def edit
+    @location = Location.find(params[:id])
+  end
+
+  # POST /locations
+  # POST /locations.xml
+  def create
+    @location = Location.new(params[:location])
+
+    respond_to do |format|
+      if @location.save
+        format.html { redirect_to(@location, :notice => 'Location was successfully created.') }
+        format.xml  { render :xml => @location, :status => :created, :location => @location }
+      else
+        format.html { render :action => "new" }
+        format.xml  { render :xml => @location.errors, :status => :unprocessable_entity }
+      end
+    end
+  end
+
+  # PUT /locations/1
+  # PUT /locations/1.xml
+  def update
+    @location = Location.find(params[:id])
+
+    respond_to do |format|
+      if @location.update_attributes(params[:location])
+        format.html { redirect_to(@location, :notice => 'Location was successfully updated.') }
+        format.xml  { head :ok }
+      else
+        format.html { render :action => "edit" }
+        format.xml  { render :xml => @location.errors, :status => :unprocessable_entity }
+      end
+    end
+  end
+
+  # DELETE /locations/1
+  # DELETE /locations/1.xml
+  def destroy
+    @location = Location.find(params[:id])
+    @location.destroy
+
+    respond_to do |format|
+      format.html { redirect_to(locations_url) }
+      format.xml  { head :ok }
+    end
+  end
+end
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Geocoder::Application.config.secret_token = 'cd95fb4bafbf2376f306aaab31fbb81b78c083b5112388bcd8d620f60e030db0309d387a25a532ca8edd826f76153482364703d6a1c616e7323bd822c9d69516'
